6c3639195922ebfafd570cce9884b2832e1edf12
beepscore <support@beepscore.com>
2016-03-05 08:41:52 -0800

    Add ContactViewController

---
+++ GoodAsOldPhones/ContactViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ContactViewController.swift
+//  GoodAsOldPhones
+//
+//  Created by Steve Baker on 3/5/16.
+//  Copyright © 2016 Beepscore LLC. All rights reserved.
+//
+
+import UIKit
+
+class ContactViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
