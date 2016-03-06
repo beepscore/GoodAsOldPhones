036c89314e3a2fafbcbc2ff2462b673a16d2d024
beepscore <support@beepscore.com>
2016-03-05 22:48:32 -0800

    In ProductViewController add productName. In ProductsTableViewController add Model object phones. Add prepareForSegue

--- GoodAsOldPhones/ProductsTableViewController.swift
+++ GoodAsOldPhones/ProductsTableViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ProductsTableViewController: UITableViewController {
 
+    // we don't have image-cell0, so use image-cell1
+    let phones = [("phone a", "image-cell1"),
+        ("phone b", "image-cell1"),
+        ("1937 Desk Phone", "image-cell2"),
+        ("phone d", "image-cell3"),
+        ("phone e", "image-cell4")
+    ]
+
+    var selectedRow : NSInteger = 0
+
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 5
     }
@@ -17,8 +27,21 @@ class ProductsTableViewController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("ProductCell",
             forIndexPath: indexPath)
-        cell.textLabel?.text = "Phone type \(indexPath.row)"
-        cell.imageView?.image = UIImage(named: "image-cell\(indexPath.row)")
+        cell.textLabel?.text = phones[indexPath.row].0
+        cell.imageView?.image = UIImage(named: phones[indexPath.row].1)
         return cell
     }
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+
+        // prepareForSegue gets called before tableView didSelectRowAtIndexPath,
+        // so use sender to get info
+        let productCell = sender as! UITableViewCell
+
+        if segue.identifier == "ShowProduct" {
+            let productViewController = segue.destinationViewController as? ProductViewController
+            productViewController?.productName = productCell.textLabel!.text
+        }
+    }
+
 }
