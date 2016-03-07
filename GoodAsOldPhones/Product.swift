600e8ee3fde4e171608f0ab51a58c010172aa9f2
beepscore <support@beepscore.com>
2016-03-06 16:11:22 -0800

    Add class Product

---
+++ GoodAsOldPhones/Product.swift
@@ -0,0 +1,16 @@
+//
+//  Product.swift
+//  GoodAsOldPhones
+//
+//  Created by Steve Baker on 3/6/16.
+//  Copyright © 2016 Beepscore LLC. All rights reserved.
+//
+
+import Foundation
+
+class Product {
+
+    var name: String?
+    var productImageName: String?
+    var cellImageName: String?
+}
